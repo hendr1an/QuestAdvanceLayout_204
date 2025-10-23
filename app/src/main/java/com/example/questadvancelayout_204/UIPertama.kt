7ae58a105808a79c79f8a66f36eb1408757d665a
hendr1an <hendriansyahgyan@gmail.com>
2025-10-24 00:54:14 +0700

    Tambah Column untuk konten teks di kartu kedua

--- app/src/main/java/com/example/questadvancelayout_204/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_204/UIPertama.kt
@@ -96,3 +96,6 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
                     .padding(all = 5.dp) // Modifier disamakan
              )
             Spacer(modifier = Modifier.width(30.dp))
+
+            Column {
+                Text(
